cc0a536d6717e1f497a001eeb4122ccdcca48cbc
areong9292 <areong9292@gmail.com>
2020-03-22 21:14:33 +0900

    Add RenderManager

---
+++ AreongEngine/RenderManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "RenderManager.h"
+
+RenderManager::RenderManager()
+{
+	MessageBox(0, L"RenderManager", L"Error", MB_OK);
+}
+
+RenderManager::~RenderManager()
+{
+	MessageBox(0, L"~RenderManager", L"Error", MB_OK);
+}
